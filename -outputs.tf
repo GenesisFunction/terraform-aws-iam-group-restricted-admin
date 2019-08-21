47ff5d2fcf6dedd334036064b003d7ce92da302b
Chris Hurst <chris.hurst@genesisfunction.com>
2019-08-21 08:56:21 -0400

    fixed outputs

--- -outputs.tf
+++ -outputs.tf
@@ -1,10 +1,10 @@
 output "iam_group_id" {
   description = "ID of IAM Group created"
-  value       = aws_iam_group.this.id
+  value       = aws_iam_group.group.id
 }
 
 output "iam_role_arn" {
   description = "ARN of IAM Group created"
-  value       = aws_iam_group.this.arn
+  value       = aws_iam_group.group.arn
 }
 
