8866ab19e8c1a5be18ac5db4d8d2c2c8643b54fe
Chris Hurst <chris.hurst@genesisfunction.com>
2019-08-21 09:16:49 -0400

    added iam role assumption policy

--- iam-role-restricted-admin.tf
+++ iam-role-restricted-admin.tf
@@ -6,5 +6,29 @@ resource "aws_iam_role" "this" {
 resource "aws_iam_role_policy_attachment" "cross_account_assume_role" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
+  assume_role_policy = data.aws_iam_policy_document.cross_account_assume_role_policy_mfa.json
+}
+
+data "aws_iam_policy_document" "cross_account_assume_role_policy_mfa" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+
+    actions = ["sts:AssumeRole"]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values = [
+        "true",
+      ]
+    }
+  }
 }
 
