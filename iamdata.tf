7ce373d6b4ffd67a17bcf1bb98be46c272d3f5b0
Wesley Kirkland <wesley.kirkland@stratusgrid.com>
2022-06-21 16:58:18 -0400

    Move IAM Data Block to another file and renamed the data block

--- iamdata.tf
+++ iamdata.tf
@@ -20,3 +20,30 @@ data "aws_iam_policy_document" "cross_account_assume_role_policy_mfa" {
     }
   }
 }
+
+data "aws_iam_policy_document" "iam_restricted_admin" {
+  statement {
+    sid = "AllowFullAdminExceptSomeWithMFA"
+    not_actions = [
+      "logs:Delete*",
+      "cloudtrail:Delete*",
+      "cloudtrail:Stop*",
+      "cloudtrail:Update*",
+      "sts:AssumeRole",
+      "sts:AssumeRoleWithSAML",
+      "sts:AssumeRoleWithWebIdentity",
+      "sts:GetFederationToken",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+  statement {
+    sid = "AllowFullAdminExceptSomeS3WithMFA"
+    not_actions = [
+      "s3:Delete*",
+      "s3:Put*",
+    ]
+    resources = var.s3_bucket_paths_to_protect
+  }
+}
